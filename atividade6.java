55f25a9880982af3ca40a727b8b0908cbad23ab0
jmaiajoao <128436278+jmaiajoao@users.noreply.github.com>
2023-04-08 21:43:35 -0300

    Add files via upload

---
+++ atividade6.java
@@ -0,0 +1,31 @@
+
+
+// programa de soluções computacionais.
+// Lista 2 ( atividade 6)
+//Aluno: João Victor Maia Leite
+//RA: 323124931
+
+import java.util.Scanner;
+
+public class atividade6 {
+    public static void main(String[] args) throws Exception {
+
+
+// Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre
+
+Scanner input = new Scanner(System.in);
+
+int num1 = input.nextInt();
+int num2 = input.nextInt();
+double num3 = input.nextDouble();
+ System.out.println("Resultado da operação é"); 
+ System.out.println( num1 + num2 * num3);
+ input.close();
+
+
+
+
+
+
+    }
+}
